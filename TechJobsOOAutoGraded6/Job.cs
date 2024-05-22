28100f2da3ff9e5b22268b97cd07d3233063c21d
Delaney Green <delaney.rosa1@gmail.com>
2024-05-22 13:52:11 -0500

    completed task 3 tests passing

--- TechJobsOOAutoGraded6/Job.cs
+++ TechJobsOOAutoGraded6/Job.cs
@@ -3,8 +3,6 @@ namespace TechJobsOOAutoGraded6
 {
 	public class Job
 	{
-        /*
-
             public int Id { get; }
             private static int nextId = 1;
             public string Name { get; set; }
@@ -14,14 +12,40 @@ namespace TechJobsOOAutoGraded6
             public CoreCompetency JobCoreCompetency { get; set; }
 
             // TODO: Task 3: Add the two necessary constructors.
+            public Job()
+            {
+                Id = nextId;
+                nextId++;
+            }
 
-            // TODO: Task 3: Generate Equals() and GetHashCode() methods.  
+            public Job(string name, Employer employer, Location location, PositionType type, CoreCompetency competency) : this ()
+            {
+                Name = name; 
+                EmployerName = employer;
+                EmployerLocation = location;
+                JobType =type;
+                JobCoreCompetency = competency;
 
-            // TODO: Task 5: Generate custom ToString() method.
-                //Until you create this method, you will not be able to print a job to the console.
+            }
+
+        // TODO: Task 3: Generate Equals() and GetHashCode() methods.  
+        public override bool Equals(object? obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+        // TODO: Task 5: Generate custom ToString() method.
+        //Until you create this method, you will not be able to print a job to the console.
 
 
-        */
+
+
+
     }
 }
 
